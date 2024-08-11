3847fbc53cf0ca8d63e37bc6e8ab5e88923c671b
BOSON-18 <deeshankbatra663@gmail.com>
2024-08-11 17:30:33 +0530

    Some basics Changes css ruined:)

--- src/data/ProjectsList.jsx
+++ src/data/ProjectsList.jsx
@@ -1,11 +1,11 @@
-import StudyNotion from "../assets/Cards/StudyNotion.jpg";
-import Netflix from "../assets/Cards/Netflix.gif";
-import Swiggy from "../assets/Cards/Swiggy.jpg";
-import Youtube from "../assets/Cards/YouTube.jpg";
+import StudyNotion from "../assets/Cards/Study.png";
+import Dashboard from "../assets/Cards/Admin.png";
+import ClumsyChats from "../assets/Cards/ClumsyChats.png";
+import Youtube from "../assets/Cards/Youtube.jpeg";
 
 export const Projects = [
   {
-    image: <img src={Netflix} alt="StudyNotion "/>,
+    image: <img src={StudyNotion} alt="StudyNotion " className="object-cover w-full h-full" />,
     name: "StudyNotion",
     techStack: [
       "Express",
@@ -19,19 +19,19 @@ export const Projects = [
     gitHub:"https://github.com/BOSON-18/StudyNotion-Mega.git"
   },
   {
-    image: <img src={Netflix} alt="StudyNotion "/>,
+    image: <img src={Youtube} alt="StudyNotion " className="object-cover w-full h-full" />,
     name: "YouTube Clone",
     techStack: ["ReactJs", "Tailwind Css", "Redux", "Google-API'S"],
     gitHub:"https://github.com/BOSON-18/namsate-youtube.git"
   },
   {
-    image: <img src={Netflix} alt="StudyStream "/>,
+    image: <img src={ClumsyChats} alt="ClumyChats " className="object-cover w-full h-full" />,
     name: "Study-Stream",
     techStack: ["Nextjs", "Tailwind Css", "Clerk", "Stream"],
-    gitHub:"https://github.com/BOSON-18/StudyStream"
+    gitHub:"https://github.com/BOSON-18/clumsyChats"
   },
   {
-    image: <img src={Netflix} alt="Dashboard "/>,
+    image: <img src={Dashboard} alt="Dashboard " className="object-cover w-full h-full" />,
     name: "Attendance Dashboard",
     techStack: ["ReactJs", "CORS", "MongoDB", "NodeJs", "Redux","Chartjs"],
     gitHub:"https://github.com/BOSON-18/newDash"
